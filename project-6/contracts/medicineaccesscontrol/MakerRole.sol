32c758d490bdb016799e64828a66c1c294af05e0
Anil Mishra <anilmishranj@gmail.com>
2019-06-04 17:29:30 -0700

    Create MakerRole.sol

---
+++ project-6/contracts/medicineaccesscontrol/MakerRole.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.24;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'MakerRole' to manage this role - add, remove, check
+contract MakerRole {
+  using Roles for Roles.Role;
+
+  // Define 2 events, one for Adding, and other for Removing
+  event MakerAdded(address indexed account);
+  event MakerRemoved(address indexed account);
+
+  // Define a struct 'farmers' by inheriting from 'Roles' library, struct Role
+  Roles.Role private farmers;
+
+  // In the constructor make the address that deploys this contract the 1st farmer
+  constructor() public {
+    _addMaker(msg.sender);
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyMaker() {
+    require(isMaker(msg.sender));
+    _;
+  }
+
+  // Define a function 'isMaker' to check this role
+  function isMaker(address account) public view returns (bool) {
+    return farmers.has(account);
+  }
+
+  // Define a function 'addMaker' that adds this role
+  function addMaker(address account) public onlyMaker {
+    _addMaker(account);
+  }
+
+  // Define a function 'renounceMaker' to renounce this role
+  function renounceMaker() public {
+    _removeMaker(msg.sender);
+  }
+
+  // Define an internal function '_addMaker' to add this role, called by 'addv'
+  function _addMaker(address account) internal {
+    farmers.add(account);
+    emit MakerAdded(account);
+  }
+
+  // Define an internal function '_removeMaker' to remove this role, called by 'removeMaker'
+  function _removeMakerMaker(address account) internal {
+    farmers.remove(account);
+    emit MakerRemoved(account);
+  }
+}
+
+    © 2019 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
