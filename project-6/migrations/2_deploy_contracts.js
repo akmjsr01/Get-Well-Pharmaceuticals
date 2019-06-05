369429c251562fd00a2ef70a0f9d3f4840bff8f8
Anil Mishra <anilmishranj@gmail.com>
2019-06-04 17:20:59 -0700

    Update 2_deploy_contracts.js

--- project-6/migrations/2_deploy_contracts.js
+++ project-6/migrations/2_deploy_contracts.js
@@ -1,14 +1,14 @@
 // migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
+var MakerRole = artifacts.require("./MakerRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
-var RetailerRole = artifacts.require("./RetailerRole.sol");
-var ConsumerRole = artifacts.require("./ConsumerRole.sol");
+var PharmacyRole = artifacts.require("./PharmacyRole.sol");
+var PatientRole = artifacts.require("./PatientRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FarmerRole);
+  deployer.deploy(MakerRole);
   deployer.deploy(DistributorRole);
-  deployer.deploy(RetailerRole);
-  deployer.deploy(ConsumerRole);
+  deployer.deploy(PharmacyRole);
+  deployer.deploy(PatientRole);
   deployer.deploy(SupplyChain);
 };
